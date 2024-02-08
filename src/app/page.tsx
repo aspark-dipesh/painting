73022fb73c1673fbf74770fc2763894e5a2f48b0
aspark-dipesh <dipesh.chaulagain@asparksys.com>
2024-02-08 10:59:24 +0545

    image extenction  changed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,21 +28,21 @@ export default function Home() {
 
             <PaintCard src='/media/images/image1.jpg' />
             <PaintCard src='/media/images/image3.jpg' />
-            <PaintCard src='/media/images/image20.jpeg' />
+            <PaintCard src='/media/images/image20.jpg' />
 
 
             <PaintCard src='/media/images/image2.jpg' />
-            <PaintCard src='/media/images/image19.jpeg' />
+            <PaintCard src='/media/images/image19.jpg' />
             <PaintCard src='/media/images/image4.jpg' />
 
-            <PaintCard src='/media/images/image8.jpeg' />
-            <PaintCard src='/media/images/image9.jpeg' />
-            <PaintCard src='/media/images/image10.jpeg' />
+            <PaintCard src='/media/images/image8.jpg' />
+            <PaintCard src='/media/images/image9.jpg' />
+            <PaintCard src='/media/images/image10.jpg' />
 
 
-            <PaintCard src='/media/images/image12.jpeg' />
-            <PaintCard src='/media/images/image13.jpeg' />
-            <PaintCard src='/media/images/image15.jpeg' />
+            <PaintCard src='/media/images/image12.jpg' />
+            <PaintCard src='/media/images/image13.jpg' />
+            <PaintCard src='/media/images/image15.jpg' />
 
 
           </div>
@@ -56,13 +56,13 @@ export default function Home() {
             </p>
           </div>
           <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 mt-2 gap-4">
-            <CategoryCard title="Hand Paintings" image="/media/images/image28.jpeg" />
-            <CategoryCard title="Acrylic Painting" image="/media/images/image18.jpeg" />
-            <CategoryCard title="Wall Panting" image="/media/images/image9.jpeg" />
-            <CategoryCard title="Oil Paintings" image="/media/images/image40.jpeg" />
-            <CategoryCard title="Spiritual" image="/media/images/image36.jpeg" />
-            <CategoryCard title="Hand Painting Signage" image="/media/images/image43.jpeg" />
-            <CategoryCard title="Portrait" image="/media/images/image38.jpeg" />
+            <CategoryCard title="Hand Paintings" image="/media/images/image28.jpg" />
+            <CategoryCard title="Acrylic Painting" image="/media/images/image18.jpg" />
+            <CategoryCard title="Wall Panting" image="/media/images/image9.jpg" />
+            <CategoryCard title="Oil Paintings" image="/media/images/image40.jpg" />
+            <CategoryCard title="Spiritual" image="/media/images/image36.jpg" />
+            <CategoryCard title="Hand Painting Signage" image="/media/images/image43.jpg" />
+            <CategoryCard title="Portrait" image="/media/images/image38.jpg" />
 
           </div>
         </div>
@@ -74,17 +74,17 @@ export default function Home() {
             <PaintCard src='/media/images/image6.jpg' />
             <PaintCard src='/media/images/image7.jpg' />
 
-            <PaintCard src='/media/images/image16.jpeg' />
+            <PaintCard src='/media/images/image16.jpg' />
             <PaintCard src='/media/images/image4.jpg' />
-            <PaintCard src='/media/images/image11.jpeg' />
+            <PaintCard src='/media/images/image11.jpg' />
 
-            <PaintCard src='/media/images/image17.jpeg' />
-            <PaintCard src='/media/images/image18.jpeg' />
-            <PaintCard src='/media/images/image19.jpeg' />
+            <PaintCard src='/media/images/image17.jpg' />
+            <PaintCard src='/media/images/image18.jpg' />
+            <PaintCard src='/media/images/image19.jpg' />
 
-            <PaintCard src='/media/images/image21.jpeg' />
-            <PaintCard src='/media/images/image22.jpeg' />
-            <PaintCard src='/media/images/image32.jpeg' />
+            <PaintCard src='/media/images/image21.jpg' />
+            <PaintCard src='/media/images/image22.jpg' />
+            <PaintCard src='/media/images/image32.jpg' />
 
 
           </div>
@@ -95,23 +95,23 @@ export default function Home() {
           <HeadLine title="Portrait" />
           <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-2">
 
-            <PaintCard src='/media/images/image25.jpeg' />
-            <PaintCard src='/media/images/image26.jpeg' />
-            <PaintCard src='/media/images/image27.jpeg' />
+            <PaintCard src='/media/images/image25.jpg' />
+            <PaintCard src='/media/images/image26.jpg' />
+            <PaintCard src='/media/images/image27.jpg' />
 
 
-            <PaintCard src='/media/images/image30.jpeg' />
-            <PaintCard src='/media/images/image29.jpeg' />
-            <PaintCard src='/media/images/image28.jpeg' />
+            <PaintCard src='/media/images/image30.jpg' />
+            <PaintCard src='/media/images/image29.jpg' />
+            <PaintCard src='/media/images/image28.jpg' />
 
 
-            <PaintCard src='/media/images/image31.jpeg' />
-            <PaintCard src='/media/images/image32.jpeg' />
-            <PaintCard src='/media/images/image33.jpeg' />
+            <PaintCard src='/media/images/image31.jpg' />
+            <PaintCard src='/media/images/image32.jpg' />
+            <PaintCard src='/media/images/image33.jpg' />
 
-            <PaintCard src='/media/images/image34.jpeg' />
-            <PaintCard src='/media/images/image35.jpeg' />
-            <PaintCard src='/media/images/image36.jpeg' />
+            <PaintCard src='/media/images/image34.jpg' />
+            <PaintCard src='/media/images/image35.jpg' />
+            <PaintCard src='/media/images/image36.jpg' />
 
 
           </div>
@@ -122,22 +122,22 @@ export default function Home() {
         <div className="mt-10">
           <HeadLine title="Trending This Month" />
           <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-2">
-            <PaintCard src='/media/images/image37.jpeg' />
-            <PaintCard src='/media/images/image38.jpeg' />
-            <PaintCard src='/media/images/image10.jpeg' />
-            <PaintCard src='/media/images/image41.jpeg' />
-            <PaintCard src='/media/images/image42.jpeg' />
-            <PaintCard src='/media/images/image24.jpeg' />
+            <PaintCard src='/media/images/image37.jpg' />
+            <PaintCard src='/media/images/image38.jpg' />
+            <PaintCard src='/media/images/image10.jpg' />
+            <PaintCard src='/media/images/image41.jpg' />
+            <PaintCard src='/media/images/image42.jpg' />
+            <PaintCard src='/media/images/image24.jpg' />
 
 
 
-            <PaintCard src='/media/images/image40.jpeg' />
-            <PaintCard src='/media/images/image43.jpeg' />
-            <PaintCard src='/media/images/image23.jpeg' />
+            <PaintCard src='/media/images/image40.jpg' />
+            <PaintCard src='/media/images/image43.jpg' />
+            <PaintCard src='/media/images/image23.jpg' />
 
-            <PaintCard src='/media/images/image15.jpeg' />
-            <PaintCard src='/media/images/image19.jpeg' />
-            <PaintCard src='/media/images/image8.jpeg' />
+            <PaintCard src='/media/images/image15.jpg' />
+            <PaintCard src='/media/images/image19.jpg' />
+            <PaintCard src='/media/images/image8.jpg' />
 
 
           </div>
