c34ab53171a3836e80d197a53c5a7a02c425913a
Dipesh <dipesh.chaulagain@asparksys.com>
2024-07-31 16:19:06 +0545

    feat: Loader added

---
+++ src/app/loading.tsx
@@ -0,0 +1,7 @@
+export default async function Loading() {
+  return (
+    <div className="w-full min-h-[70vh] flex justify-center items-center">
+      <div className="loader"></div>
+    </div>
+  );
+}
